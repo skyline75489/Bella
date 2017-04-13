ccce488b44be78350601c3637c2df4b24bfda9c0
skyline75489 <skyline75489@outlook.com>
2017-04-13 20:28:10 +0800

    Fix warning

--- sundown/Bella.c
+++ sundown/Bella.c
@@ -6,9 +6,9 @@ void string_to_buf(struct buf *ib, const char *i) {
     bufgrow(ib, strlen(i));
     
     ib->size = strlen(i);
-    ib->data = strdup(i);
+    ib->data = (uint8_t *)strdup(i);
 }
 
 const char *buf_to_string(struct buf *ob) {
-    return strdup(ob->data);
+    return strdup((char *)ob->data);
 }
